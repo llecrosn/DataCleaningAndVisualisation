b10b2cdc723f6e68e06d02bc0be1e1d037ef5e19
plamour <pierre.lamour@edu.univ-fcomte.fr>
2026-09-21 11:05:42 +0200

    commit

--- Analyze/dataset_cleaner.py
+++ Analyze/dataset_cleaner.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# Try this first
+df = pd.read_csv("taxomany.tsv", sep="\t", skiprows=1)
 
-
-
+print(df.shape)
+print(df.head())
+print(df.columns.tolist())
+print(df.dtypes)
