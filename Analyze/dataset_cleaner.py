f3d52e466b1d3ca70a0972eb8878baa955fe5870
plamour <pierre.lamour@edu.univ-fcomte.fr>
2026-09-21 11:12:46 +0200

    commit

--- Analyze/dataset_cleaner.py
+++ Analyze/dataset_cleaner.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
 # Try this first
-df = pd.read_csv("taxomany.tsv", sep="\t", skiprows=1)
+df = pd.read_csv("./raw_dataset/Bacteria Taxonomy dataset/data/taxonomy.tsv", sep="\t", names=["Feature_ID", "Taxon", "Confidence"], header=0)
 
-print(df.shape)
-print(df.head())
-print(df.columns.tolist())
-print(df.dtypes)
+
+ranks = ["Domain","Phylum","Class","Order","Family","Genus","Species"]
+df[ranks] = df["Taxon"].str.split(";", expand=True)
+
+print(df["Phylum"].value_counts().head(15))
+
+print(df["Species"].isna().sum(), "ASVs unresolved to species")
+
+print(df["Confidence"].describe())
